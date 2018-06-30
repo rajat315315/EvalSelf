ee49620e85b5789a3e8a22c83ccf69d35ecedebc
rajat.jain <rajat.jain@naukri.com>
2018-07-01 05:10:44 +0530

    removed unecessary chart

--- app.r
+++ app.r
@@ -23,10 +23,9 @@ ui <- dashboardPage(
       column(3, textInput("skill", label = h3("skill"), value = "Enter text...")),
       column(3, textInput("degree", label = h3("degree"), value = "B.Tech/B.E.")),
       
-      h3(textOutput("caption", container = span)),
+      valueBoxOutput("caption"),
       h3(tableOutput("plot6")),
       box(plotlyOutput("avgctcminmax", height = 450)),
-      box(plotlyOutput("plot2", height = 250)),
       box(plotlyOutput("plot3", height = 250)),
       box(plotlyOutput("plot4", height = 250)),
       box(plotlyOutput("topPaidSkills", height = 250))
@@ -39,8 +38,21 @@ ui <- dashboardPage(
 server <- function(input, output) {
   set.seed(122)
   histdata <- rnorm(500)
-  data <- read.csv(file = '/home/aman/mappedData.csv', header = TRUE, fill = TRUE)
+  data <- read.csv(file = '/home/rajat/Downloads/mappedData.csv', header = TRUE, fill = TRUE)
   #data = as.data.frame(data)
+  
+  output$caption <- renderValueBox({
+    h <- new_handle()
+    handle_setform(h,
+                   college = "118",
+                   branch = "12",
+                   batch = "2012"
+    )
+    req <- curl_fetch_memory("http://192.168.173.56:8092/hackathon/getAvgCtc", handle = h)
+    json = rawToChar(req$content)
+    #json = 1
+    valueBox(json,"OUTPUT", icon = icon("stats",lib='glyphicon'),color = "purple")  
+  })
   
   output$topPaidSkills <- renderPlotly({
     date = as.double(input$year)
@@ -188,35 +200,6 @@ server <- function(input, output) {
              xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
              yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
   })
-  
-  output$plot2 <- renderPlotly({
-    
-    x <- c(1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012)
-    roW <- c(219, 146, 112, 127, 124, 180, 236, 207, 236, 263, 350, 430, 474, 526, 488, 537, 500, 439)
-    China <- c(16, 13, 10, 11, 28, 37, 43, 55, 56, 88, 105, 156, 270, 299, 340, 403, 549, 499)
-    data <- data.frame(x, roW, China)
-    
-    p <- plot_ly(data, x = ~x, y = ~roW, type = 'bar', name = 'Rest of the World',
-                 marker = list(color = 'rgb(55, 83, 109)')) %>%
-      add_trace(y = ~China, name = 'China', marker = list(color = 'rgb(26, 118, 255)')) %>%
-      layout(title = 'US Export of Plastic Scrap',
-             xaxis = list(
-               title = "",
-               tickfont = list(
-                 size = 14,
-                 color = 'rgb(107, 107, 107)')),
-             yaxis = list(
-               title = 'USD (millions)',
-               titlefont = list(
-                 size = 16,
-                 color = 'rgb(107, 107, 107)'),
-               tickfont = list(
-                 size = 14,
-                 color = 'rgb(107, 107, 107)')),
-             legend = list(x = 0, y = 1, bgcolor = 'rgba(255, 255, 255, 0)', bordercolor = 'rgba(255, 255, 255, 0)'),
-             barmode = 'group', bargap = 0.15, bargroupgap = 0.1)
-  })
-  
   
 }
 
